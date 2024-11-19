1941fb90072569009fff3090913409ce6c37a99e
ravish463 <ravish.industill@gmail.com>
2024-11-19 23:45:41 +0530

    added teams contacts and invoices page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import {Routes,Route} from 'react-router-dom'
 import TopBar from './scenes/global/topBar'
 import  Dashboard  from './scenes/dashboard';
 import Sidebar from './scenes/global/sideBar'
-//import Team from './scenes/team'
-//import Invoices from './scenes/invoices'
-//import Contacts from './scenes/contacts'
+import Team from './scenes/team'
+import Invoices from './scenes/invoices'
+import Contacts from './scenes/contacts'
 //import Bar from './scenes/bar'
 //import Form from './scenes/form'
 //import Line from './scenes/line'
@@ -27,9 +27,9 @@ function App() {
               <TopBar />
               <Routes>
                 <Route  path = "/" element = {<Dashboard />} />
-                {/* <Route  path = "/team" element = {<Team />} /> */}
-                {/* <Route  path = "/contacts" element = {<Contacts />} /> */}
-                {/* <Route  path = "/invoices" element = {<Invoices />} /> */}
+                <Route  path = "/team" element = {<Team />} />
+                <Route  path = "/contact" element = {<Contacts />} />
+                <Route  path = "/invoices" element = {<Invoices />} />
                 {/* <Route  path = "/bar" element = {<Bar />} /> */}
                 {/* <Route  path = "/form" element = {<Form />} /> */}
                 {/* <Route  path = "/line" element = {<Line />} /> */}
